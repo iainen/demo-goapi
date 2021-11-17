1f1c365812a4026c66bd48e7d03e37d305a62754
yi.zhang <yi.zhang@broadfun.cn>
2021-11-17 16:12:07 +0800

    init

---
+++ scripts/update.sh
@@ -0,0 +1,42 @@
+#!/usr/bin
+
+export BASEDIR=$(dirname "$0")/..
+export TEMPDIR="$BASEDIR"/temp
+export PROTOCOLDIR="$BASEDIR"/temp/protocols
+
+echo "$BASEDIR"
+echo "$TEMPDIR"
+echo "$PROTOCOLDIR"
+
+
+function initProtocolHome(){
+  if [[ -d "$PROTOCOLDIR" ]]; then
+    rm -rf "$PROTOCOLDIR}"
+  fi
+  mkdir -p "$PROTOCOLDIR"
+}
+
+function cleanHistoryCodes(){
+  rm -rf "$BASEDIR"/demo
+}
+
+function generateCodes(){
+  protoc \
+  --proto_path="$PROTOCOLDIR"/demo-protocol \
+  --proto_path="$PROTOCOLDIR" \
+  --go_out="$BASEDIR" \
+  --go-grpc_out="$BASEDIR" \
+  "$PROTOCOLDIR"/demo-protocol/*/*.proto
+
+  mv "$BASEDIR"/goapi/demo "$BASEDIR"/ \
+  && rm -rf "$BASEDIR"/goapi && rm -rf "$TEMPDIR"
+  go mod tidy
+}
+
+initProtocolHome
+cleanHistoryCodes
+
+cp -r "$BASEDIR"/../demo-protocol "$PROTOCOLDIR"/
+cp -r "$BASEDIR"/../sky-apm-demo/service/protobuf/google1 "$PROTOCOLDIR"/google
+
+generateCodes
